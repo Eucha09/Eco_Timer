c5edd746333da51db04f026174db8af3d34bef12
define_chan / 정의찬 <45040732+Eucha09@users.noreply.github.com>
2022-11-09 09:39:19 +0900

    origin commit2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -69,7 +69,7 @@ class _HomePageState extends State<HomePage> {
           size: 225.0,
         ),
         min: 0,
-        max: 60,
+        max: 360,
         initialValue: 0,
         innerWidget: (double value) {
           return Center(
